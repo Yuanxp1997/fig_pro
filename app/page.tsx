0603ec4bceeeb19e007150c4df4082d7f277c601
Yuanxp1997 <127631889+Yuanxp1997@users.noreply.github.com>
2024-03-08 18:56:30 -0700

    feat: add navbar icons and select shape function

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,67 @@ import Navbar from "@/components/Navbar";
 
 import SidebarLeft from "@/components/SidebarLeft";
 import SidebarRight from "@/components/SidebarRight";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { fabric } from "fabric";
 import {
   handleCanvasMouseDown,
   handleResize,
   initializeFabric,
 } from "@/lib/canvas";
+import { ActiveElement } from "@/types/type";
+import { useMutation } from "@/liveblocks.config";
+import { defaultNavElement } from "@/constants";
+import { handleImageUpload } from "@/lib/shapes";
 
 export default function Home() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const fabricRef = useRef<fabric.Canvas | null>(null);
   const isDrawing = useRef(false);
   const shapeRef = useRef<fabric.Object | null>(null);
-  const selectedShapeRef = useRef<string | null>("rectangle");
+  const selectedShapeRef = useRef<string | null>(null);
+  const imageInputRef = useRef<HTMLInputElement | null>(null);
+  const [activeElement, setActiveElement] = useState<ActiveElement>({
+    name: "",
+    value: "",
+    icon: "",
+  });
+
+  const handleActiveElement = (element: ActiveElement) => {
+    setActiveElement(element);
+
+    switch (element?.value) {
+      case "reset":
+        fabricRef.current?.clear();
+        setActiveElement(defaultNavElement);
+        break;
+      case "delete":
+        setActiveElement(defaultNavElement);
+        break;
+      case "image":
+        imageInputRef.current?.click();
+        /**
+         * set drawing mode to false
+         * If the user is drawing on the canvas, we want to stop the
+         * drawing mode when clicked on the image item from the dropdown.
+         */
+        isDrawing.current = false;
+        if (fabricRef.current) {
+          // disable the drawing mode of canvas
+          fabricRef.current.isDrawingMode = false;
+        }
+        break;
+
+      // for comments, do nothing
+      case "comments":
+        break;
+
+      default:
+        // set the selected shape to the selected element
+        selectedShapeRef.current = element?.value as string;
+        break;
+    }
+  };
+
   useEffect(() => {
     const canvas = initializeFabric({ canvasRef, fabricRef });
     canvas.on("mouse:down", (options) => {
@@ -35,7 +82,11 @@ export default function Home() {
   }, []);
   return (
     <div className="h-[100vh] flex flex-col w-full">
-      <Navbar />
+      <Navbar
+        activeElement={activeElement}
+        handleActiveElement={handleActiveElement}
+        imageInputRef={imageInputRef}
+      />
       <section className="flex-1  flex">
         <SidebarLeft />
         <Live canvasRef={canvasRef} />
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,12 +2,86 @@
 import React from "react";
 import LiveAvatars from "./avatar/LiveAvatars";
 import Image from "next/image";
+import { navElements } from "@/constants";
+import { Button } from "./ui/button";
+import { ActiveElement, NavbarProps } from "@/types/type";
+import ShapesMenu from "./ShapesMenu";
+import { NewThread } from "./comments/NewThread";
 
-const Navbar = () => {
+const Navbar = ({
+  activeElement,
+  handleActiveElement,
+  imageInputRef,
+  handleImageUpload,
+}: NavbarProps) => {
+  const isActive = (value: string | Array<ActiveElement>) => {
+    return (
+      (activeElement && activeElement.value === value) ||
+      (Array.isArray(value) &&
+        value.some((val) => val?.value === activeElement?.value))
+    );
+  };
+  const handleClick = (item: any) => {
+    if (item.name !== "Rectangle") {
+      handleActiveElement(item);
+    }
+  };
   return (
-    <nav className="w-full flex select-none items-center justify-between gap-4 bg-primary-black px-5 py-2 text-white">
+    <nav className="w-full flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white">
       <Image src="/assets/logo.svg" alt="FigPro Logo" width={58} height={20} />
-
+      <ul className="flex items-center">
+        {navElements.map((item) => (
+          <li
+            key={item.name}
+            className={`group h-14 w-12 my-2  flex justify-center items-center cursor-pointer 
+            ${
+              isActive(item.value)
+                ? "bg-primary-green"
+                : "hover:bg-primary-grey-200"
+            }
+            `}
+            onClick={() => handleClick(item)}
+          >
+            {Array.isArray(item.value) ? (
+              <ShapesMenu
+                item={item}
+                activeElement={activeElement}
+                handleActiveElement={handleActiveElement}
+                imageInputRef={imageInputRef}
+                handleImageUpload={handleImageUpload}
+              />
+            ) : item.name === "Comments" ? (
+              <NewThread>
+                <Button className="relative w-5 h-5 object-contain">
+                  <Image
+                    src={item.icon}
+                    alt={item.name}
+                    fill
+                    className={isActive(item.value) ? "invert" : ""}
+                  />
+                </Button>
+              </NewThread>
+            ) : (
+              <Button
+                className={
+                  item.name === "Reset"
+                    ? "relative w-4 h-5 object-contain"
+                    : Array.isArray(item.value)
+                    ? "relative w-5 h-5 object-contain"
+                    : "relative w-5 h-5 object-contain"
+                }
+              >
+                <Image
+                  src={item.icon}
+                  alt={item.name}
+                  fill
+                  className={isActive(item.value) ? "invert" : ""}
+                />
+              </Button>
+            )}
+          </li>
+        ))}
+      </ul>
       <LiveAvatars />
     </nav>
   );
